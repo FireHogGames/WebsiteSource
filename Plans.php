6f6db8c9fbcc6457fada93166dced8bf591ad16f
hidde loman <hidde01@live.nl>
2019-09-20 21:52:45 +0200

    Added discord memberlist feature, Made standard plan 2,99 instead of free

--- Plans.php
+++ Plans.php
@@ -7,19 +7,7 @@
 	<link rel="stylesheet" type="text/css" href="./css/loader.css">
 	<link rel="stylesheet" type="text/css" href="./css/plans.css">
 
-	<script>
-		var myVar;
-		
-		function myFunction() {
-		  myVar = setTimeout(showPage, 500);
-		}
-		
-		function showPage() {
-		  document.getElementById("loader").style.display = "none";
-		  document.getElementById("myDiv").style.display = "block";
-		  document.getElementById("loaderTitle").style.display = "none";
-		}
-	</script>
+	<script src="./js/main.js"></script>
 </head>
 <body onload="myFunction()" style="margin: 0px; font-family: Arial; background-color: #1b4d83;">
 	<h1 style="text-align: center; font-size: 90px; margin-top: 8%; color: white;" id="loaderTitle">NewWave cloud</h1>
@@ -27,8 +15,8 @@
 
 	
 	<div class="animated fadeInDown slower" style="display:none;" id="myDiv">
-		<div class="company">
-			<strong><a href="index">NewWave Cloud</a></strong>
+		<div style="text-align: center;" class="company">
+			<strong><a  href="index">NewWave Cloud</a></strong>
 		</div>
 		<div class="topnav">
 			<strong>
@@ -36,17 +24,25 @@
 			<a class="active" href="plans">Plans</a>
 			<a  class="login" href="#panel">Panel</a>
 			<a  class="login" href="register">Register</a>
+			<button class="login" onclick="opendiscordchat()"><strong>Our Discord</strong></button>
 			</strong>
 		</div>
+
+		<div class="animated fadeInDown" style="float: right; display: none; position:absolute; padding: 14px 16px;" id="discordchat">
+			<iframe src="https://discordapp.com/widget?id=624653915169751060&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
+		</div>
+
+		<div class="item-holder">
 				<div class="item-standard">
 					<div class="item-text-standard">
 						<h1>Standard plan!</h1>
 						<p>5GB SSD storage!</p>
 						<p>Access files everywhere!</p>
+						<p>24/7 customer service for free!</p>
 						
 					</div>
-					<div style="margin-top: 85%;" class="price">
-							<a href="#Standard">Order now<br> FREE</a>
+					<div style="margin-top: 60%;" class="price">
+							<a href="#Standard">Order now<br> €2,99</a>
 						</div>
 				</div>
 				<div class="item-standard">
@@ -89,6 +85,7 @@
 							<a href="#Ultimate">Order now<br> €14,99/month</a>
 						</div>
 				</div>
+			</div>
 	</div>
 </body>
 </html>
--- Register.php
+++ Register.php
@@ -7,19 +7,7 @@
 	<link rel="stylesheet" type="text/css" href="./css/loader.css">
 	<link rel="stylesheet" type="text/css" href="./css/register.css">
 
-	<script>
-		var myVar;
-		
-		function myFunction() {
-		  myVar = setTimeout(showPage, 500);
-		}
-		
-		function showPage() {
-		  document.getElementById("loader").style.display = "none";
-		  document.getElementById("myDiv").style.display = "block";
-		  document.getElementById("loaderTitle").style.display = "none";
-		}
-	</script>
+	<script src="./js/main.js"></script>
 </head>
 <body onload="myFunction()" style="margin: 0px; font-family: Arial; background-color: #1b4d83;">
 	<h1 style="text-align: center; font-size: 90px; margin-top: 8%; color: white;" id="loaderTitle">NewWave cloud</h1>
@@ -27,7 +15,7 @@
 
 	
 	<div class="animated fadeInDown slower" style="display:none;" id="myDiv">
-		<div class="company">
+		<div style="text-align: center;" class="company">
 			<strong><a href="index">NewWave Cloud</a></strong>
 		</div>
 		<div class="topnav">
@@ -36,7 +24,12 @@
 			<a href="plans">Plans</a>
 			<a class="login" href="#panel">Panel</a>
 			<a class="login" href="register">Register</a>
+			<button class="login" onclick="opendiscordchat()"><strong>Our Discord</strong></button>
 			</strong>
+		</div>
+
+		<div class="animated fadeInDown" style="float: right; display: none; position:absolute; padding: 14px 16px;" id="discordchat">
+			<iframe src="https://discordapp.com/widget?id=624653915169751060&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
 		</div>
 
 		<div class="MainBody">
